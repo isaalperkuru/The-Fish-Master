ceccdb6a53680c1cf77f688a02244baffd4b0af5
Alper <alperkuru98@hotmail.com>
2021-09-21 15:20:42 +0300

    Commit 1

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Fish fishPrefab;
+    [SerializeField] private Fish.FishType[] types;
+    void Awake()
+    {
+        for (int i = 0; i < types.Length; i++)
+        {
+            int num = 0;
+            while(num < types[i].fishCount)
+            {
+                Fish fish = UnityEngine.Object.Instantiate<Fish>(fishPrefab);
+                fish.Type = types[i];
+                fish.ResetFish();
+                num++;
+            }
+        }
+    }
+}
